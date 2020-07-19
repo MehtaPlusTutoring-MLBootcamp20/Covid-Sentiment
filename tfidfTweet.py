3c5ab9653e3c3a90ec54b9a5f52088ff8ab33ee2
arianna-caow <66382962+arianna-caow@users.noreply.github.com>
2020-07-18 23:50:27 -0700

    trying to fix tfidf stuff

--- tfidfTweet.py
+++ tfidfTweet.py
@@ -8,7 +8,7 @@ def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
 start_date = date(2020, 4, 12)
-end_date = date(2020, 4, 13) #end date, datetime.date(datetime.now()) (this second option is dynamic and changes by date but depends on timezone)
+end_date = date(2020, 7, 13) #end date, datetime.date(datetime.now()) (this second option is dynamic and changes by date but depends on timezone)
 
 state_names = ["Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
 states = pd.get_dummies(state_names)
@@ -26,8 +26,13 @@ for date in daterange(start_date, end_date):
 state = pd.get_dummies(articles['location'])
 articles = pd.concat([articles, state], axis = 1)
 articles = articles.drop(columns = ['Unnamed: 0'])
+articlesFile = "articles.csv"
+articles.to_csv(articlesFile)
+
 #print(articles.head())
 #print (articles.tail(10))
+#print (articles.columns)
+#print(articles.head(150))
 
 import sklearn
 from sklearn import feature_extraction
@@ -47,28 +52,35 @@ tfidf = tf_counter.fit(articles['tweet'].apply(lambda x: np.str_(articles['tweet
 df = articles
 data = []
 
-print(df['location'])
+#print(df['location'])
 
-for date in daterange(start_date, end_date):
-    '''
-    words="finalTweets"+date.strftime("%B%d").lower()+".csv"
-    df = pd.read_csv(words)
-    df = df.drop(columns = ['Unnamed: 0'])
-    '''
-    #df['tfidf'] = tf_counter.transform(df['tweet'])
-    #df[['location','tfidf']].groupby('location').mean()
-    #print(df[['location','tfidf']].groupby('location').mean())
-    #print(df.dtypes)
-    for state in df['location'].unique():
-        avgtfidf = df[(df['location']==state) & (df['date'] == date)].apply(lambda x: tf_counter.transform([x['tweet']]), axis = 1).sum()
-        avgtfidf = avgtfidf/len(df[(df['location']==state) & (df['date'] == date)])
-        print(type(avgtfidf))
-        #print(avgtfidf)
-        print(state)
-        feature = avgtfidf.toarray()
-        #df = np.append(feature,[value])
-        data.append(feature)
-        
+#for date in daterange(start_date, end_date):
+'''
+words="finalTweets"+date.strftime("%B%d").lower()+".csv"
+df = pd.read_csv(words)
+df = df.drop(columns = ['Unnamed: 0'])
+'''
+#df['tfidf'] = tf_counter.transform(df['tweet'])
+#df[['location','tfidf']].groupby('location').mean()
+#print(df[['location','tfidf']].groupby('location').mean())
+#print(df.dtypes)
+
+for state in df['location'].unique():
+    for date in daterange(start_date, end_date):
+        thisTweet="finalTweets"+date.strftime("%B%d").lower()+".csv"
+        twee = pd.read_csv(thisTweet)
+        #print(twee.columns)
+        #print(twee.head(10))
+        if twee['location'].str.contains(state).any():
+            avgtfidf = df[(df['location']==state) & (df['date'] == date)].apply(lambda x: tf_counter.transform([x['tweet']]), axis = 1).sum()
+            avgtfidf = avgtfidf/len(df[(df['location']==state) & (df['date'] == date)])
+            print(type(avgtfidf))
+            #print(avgtfidf)
+            print(state)
+            feature = avgtfidf.toarray()
+            #df = np.append(feature,[value])
+            data.append(feature)
+            #break
 
 # Get the words corresponding to the vocab index
 tf_counter.get_feature_names()
