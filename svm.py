ba6408aab523f1331d1cd764ea58341c45eef1a2
arianna-caow <66382962+arianna-caow@users.noreply.github.com>
2020-07-21 23:23:20 -0700

    svm

--- svm.py
+++ svm.py
@@ -65,10 +65,65 @@ print(report)
 #print('neutral: ', report[0])
 #print('negative: ', report[-1])
 #----------------------------------------------------------------------------
-import pickle
+#import pickle
 # pickling the vectorizer
-pickle.dump(vectorizer, open('vectorizer.sav', 'wb'))
+#pickle.dump(vectorizer, open('vectorizer.sav', 'wb'))
 
 # pickling the model
-pickle.dump(classifier_linear, open('classifier.sav', 'wb'))
+#pickle.dump(classifier_linear, open('classifier.sav', 'wb'))
 #----------------------------------------------------------------------------
+
+import matplotlib.pyplot as plt 
+import datetime
+
+dates = y_train['date'].unique()
+
+converted_dates = list(map(datetime.datetime.strptime, dates, len(dates)*['%Y-%m-%d']))
+#y_axis = y_train['Confirmed']
+state = 'Texas'
+stateX = X_train.toarray()[(y_train['location']==state)] #& (y_train['date']< '2020-04-19')]
+#ylist = classifier_linear.predict(stateX)
+#print(ylist)
+from matplotlib import pyplot as plt
+
+
+
+y_df = y_train[(y_train['location']==state)] #& (y_train['date']< '2020-04-19')]
+y_df['date'] = pd.to_datetime(y_df['date'])
+y_df['predicted_sent'] = classifier_linear.predict(stateX)
+y_df = y_df.groupby('date').mean()
+print(type(y_df))
+plt.scatter(y_df['date'],y_df['predicted_sent'])
+plt.yticks(rotation=90)
+plt.show()
+
+'''
+import matplotlib.pyplot as plt 
+from datetime import date, timedelta, datetime
+
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+start_date = date(2020, 5, 14)
+end_date = date(2020, 7, 13)
+
+
+ylist = []
+averagesent = []
+for datess in daterange(start_date, end_date):
+    after = datess + timedelta(1)
+    y_axis = y_train['Confirmed']
+    state = 'Texas'
+    stateX = X_train.toarray()[(y_train['location']==state) & (y_train['date'] == (datess.strftime("%Y-%m%-d")))]
+    ylist = classifier_linear.predict(stateX)
+    print(ylist)
+    y_df = y_train[(y_train['location']==state)] #& (y_train['date']< '2020-04-19')]
+    y_df['date'] = pd.to_datetime(y_df['date'])
+    average = sum(ylist)/len(ylist)
+    averagesent.append(average)
+    plt.scatter(datess.strftime("%Y-%m%-d"), average)
+print(averagesent)
+plt.scatter(y_df['date'],averagesent)
+plt.yticks(rotation=90)
+plt.show()'''
